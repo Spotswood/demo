82ed51fcb67ed310cdf6e4db0154018f9d501980
orestis <o.tsapogas@gmail.com>
2019-08-18 20:31:07 +0100

    Adds first version of API

---
+++ src/test/java/com/orestistsapogas/demo/MyFirstSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orestistsapogas.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
